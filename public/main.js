4c6293d1bdc3e215e53ca8885775309f038293d2
pray <731586220@qq.com>
2020-06-11 17:38:51 +0800

    init

--- public/main.js
+++ public/main.js
@@ -1,93 +1,93 @@
-let n = 1;
-getPage.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", `/page${n+1}`);
-  request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const array = JSON.parse(request.response);
-      array.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item.id;
-        xxx.appendChild(li);
-      });
-      n+=1
-    }
-  };
-  request.send();
-};
-getJSON.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("get", "/5.json");
-  request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      console.log(typeof request.response);
-      console.log(request.response);
-      const bool = JSON.parse(request.response);
-      console.log(typeof bool);
-      console.log(bool);
-    }
-  };
-  request.send();
-};
-getXML.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/4.xml");
-  request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const dom = request.responseXML;
-      const text = dom.getElementsByTagName("warning")[0].textContent;
-      console.log(text.trim());
-    }
-  };
-  request.send();
-};
-getHTML.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/3.htm");
-  request.onload = () => {
-    // 创建 div 标签
-    const div = document.createElement("div");
-    // 填写 div 内容
-    div.innerHTML = request.response;
-    // 插入到身体里
-    document.body.appendChild(div);
-  };
-  request.onerror = () => {};
-  request.send();
-};
-getJS.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/2.js");
-  request.onload = () => {
-    // 创建 script 标签
-    const script = document.createElement("script");
-    // 填写 script 内容
-    script.innerHTML = request.response;
-    // 插到身体里
-    document.body.appendChild(script);
-  };
-  request.onerror = () => {};
-  request.send();
-};
 
 getCSS.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/style.css"); // readyState = 1
+  //step1 创建request对象
+  const request = new XMLHttpRequest()
+  //step2 初始化http请求参数  参数一：请求方法 参数二：请求url
+  request.open('GET', "/style.css") //readyState = 1
+  //step3 配置 请求成功事件 和 请求失败事件
   request.onreadystatechange = () => {
-    console.log(request.readyState);
-    // 下载完成，但不知道是成功 2xx 还是失败 4xx 5xx
-    if (request.readyState === 4) {
+    console.log(request.readyState)
+    if (request.readyState === 4) { //下载完成（但是不知道是成功2xx 还是失败404）
       if (request.status >= 200 && request.status < 300) {
-        // 创建 style 标签
-        const style = document.createElement("style");
-        // 填写 style 内容
-        style.innerHTML = request.response;
-        // 插到头里面
-        document.head.appendChild(style);
-      } else {
-        alert("加载 CSS 失败");
+        const style = document.createElement('style') //创建 style 标签
+        style.innerHTML = request.response            //填写 style 内容
+        document.head.appendChild(style)              //插入 head  里面
+      }
+      else {
+        alert("加载 CSS 失败")
       }
     }
-  };
-  request.send(); // readyState = 2
-};
+  }
+  //step4 发送请求
+  request.send();
+}
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/2.js')
+  request.onload = () => {
+    const script = document.createElement('script')
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  }
+  request.onerror = () => {
+  }
+  request.send();
+}
+
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/3.html')
+  request.onload = () => {
+    const div = document.createElement('div')
+    div.innerHTML = request.response
+    document.body.appendChild(div)
+  }
+  request.onerror = () => {
+  }
+  request.send();
+}
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', '/4.xml')
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML
+      const text = dom.getElementsByTagName('warning')[0].textContent
+      console.log(text.trim())
+    }
+  }
+  request.send()
+}
+
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', '/5.json')
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const obj = JSON.parse(request.response)
+      myName.innerHTML = obj.name;
+    }
+  }
+  request.send()
+}
+let n = 2;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', `/page${n}`)
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const array = JSON.parse(request.response)
+      array.forEach(item => {
+        const li = document.createElement("li")
+        li.textContent = item.id
+        xxx.appendChild(li)
+      })
+      n = n + 1;
+    }
+
+  }
+  request.send()
+}
+
